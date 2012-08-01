5273968cdee6afe2250f0a79515d2888a5cd66cb
cgiacomi <christiangiacomi@gmail.com>
2012-08-01 22:54:19 +0200

    Initial commit

---
+++ FluidLinq/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluidLinq")]
+[assembly: AssemblyDescription("Linq extension library which allow a more fluid way to express Linq queries when parsing XML to deserialize to Objects")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Christian Giacomi")]
+[assembly: AssemblyProduct("FluidLinq")]
+[assembly: AssemblyCopyright("Copyright © 2012 Christian Giacomi http://www.christiangiacomi.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8e1f366-9b29-4177-98d5-cc50df4edaa1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ README.md
@@ -0,0 +1,27 @@
+# FluidLinq
+
+FluidLinq is a small Linq extension library that makes it very easy to parse XML. 
+
+The library is meant to be used to deserialize objects from XML. It reduces the code that needs to be written and ensures that you do not run into casting exception.
+
+
+## Requirements
+
+FluidLinq requires .NET4
+
+The project requires Visual Studio 2010
+
+
+## Installation
+
+Presently you need to download the source code and build the project but it will be added to NuGet shortly...
+
+
+## Use
+
+Coming soon...
+
+
+## License 
+
+FluidLinq is released under the Apache License, Version 2.0
